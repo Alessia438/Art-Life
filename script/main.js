1473eebad419aed55bc695d92dfb598b32e93aff
Alessia438 <56439170+Alessia438@users.noreply.github.com>
2020-12-10 12:14:12 +0100

    tentativo importo octokit

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
- function myFunction() {
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
+function myFunction() {
 	var x = document.getElementById("myTopnav");
 	if (x.className === "topnav") {x.className += " responsive";} 
 	else {x.className = "topnav";}
