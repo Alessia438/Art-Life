95df74a209fb56941ff07641c3d48d6f9a187146
Alessia438 <56439170+Alessia438@users.noreply.github.com>
2020-12-11 23:10:27 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 /*import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 var response = fetch('/cookbook-cakes/articles/issue1_art1/art1-bbc1.html');
 var y = 2*1;*/
-
+var luckyEditions = document.getElementById("author");
 
 function myFunction() {
 	var x = document.getElementById("myTopnav");
