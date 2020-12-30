6ac5a64c9324434dae49f4b34bbce89fc8001d34
Alessia438 <56439170+Alessia438@users.noreply.github.com>
2020-12-30 12:33:35 +0100

    tolto richiamo a caption

--- script/main.js
+++ script/main.js
@@ -700,7 +700,6 @@ function showCurSlide(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("slideDemo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
